4d645d67a2b6df55ca0744bcb20c709272cc76b0
Binnyboy1 <ibinstock@outlook.com>
2021-11-13 11:29:51 -0800

    3D version

---
+++ Learning/Assets/Scripts/CollisionPlzWork.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionPlzWork : MonoBehaviour
+{
+    public Text LogCollisionEnter;
+    public Text LogCollisionStay;
+    public Text LogCollisionExit;
+
+    void Start()
+    {
+        GameObject go = GameObject.Find("Note");
+        if (go)
+        {
+            Debug.Log(go.name);
+        }
+        else
+        {
+            Debug.Log("No game object called Indicator_note found");
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision != null)
+        {
+            LogCollisionEnter.text = "On Trigger Enter: " + collision.GetComponent<Collider>().name;
+        }
+    }
+    /*
+    private void OnTriggerStay(Collider collision)
+    {
+        LogCollisionStay.text = "On Trigger stay: " + gameObject.GetComponent<Indicator_note>().name;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        LogCollisionExit.text = "On Trigger exit: " + gameObject.GetComponent<Collider>().name;
+    }
+    */
+}
