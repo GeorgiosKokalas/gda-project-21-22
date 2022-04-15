9d8a2f71c312468dfda8c7dc08f87af0a11b8ebb
gilber12 <78660678+gilber12@users.noreply.github.com>
2022-04-14 19:44:58 -0500

    Made outline for beatmap generation, moved timelines into folders

--- Learning/Assets/Scripts/BeatScroller.cs
+++ Learning/Assets/Scripts/BeatScroller.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
 
 public class BeatScroller : MonoBehaviour
 {
@@ -20,7 +22,9 @@ public class BeatScroller : MonoBehaviour
     public GameObject midCol;
     public GameObject rightCol;
 
-    private Quaternion rotation; 
+    private Quaternion rotation;
+
+    public MidiFile mapTrack;
 
     // public Player Frog;
 
@@ -29,22 +33,23 @@ public class BeatScroller : MonoBehaviour
     {
         beatTempo = beatTempo / 20f;
         rotation = leftSpawn.transform.rotation;
+        generateBeatmap();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!hasStarted)
+        if (!hasStarted)
         {
-            if(Input.anyKeyDown)
+            if (Input.anyKeyDown)
             {
                 hasStarted = true;
             }
         } else
         {
-            foreach(Transform child in transform)
+            foreach (Transform child in transform)
             {
-                child.transform.position += child.transform.forward *  beatTempo * Time.deltaTime;
+                child.transform.position += child.transform.forward * beatTempo * Time.deltaTime;
             }
         }
 
@@ -63,5 +68,14 @@ public class BeatScroller : MonoBehaviour
             GameObject newNote = Instantiate(notePrefab, new Vector3(rightSpawn.transform.position.x, rightSpawn.transform.position.y, rightSpawn.transform.position.z), rotation, transform);
             newNote.tag = "right_note";
         }
+    }
+
+    //CUSTOM-MADE FUNCTIONS
+    void generateBeatmap()
+    {
+        //create array of timestamps representing each note
+        //procede through array in order (should already be sorted)
+        //when current time = note's time, spawn note (with an offset to account for the time to travel from spawn to the hit zone, and a grace window to allow for error), then advance through array
+
     }
 }
